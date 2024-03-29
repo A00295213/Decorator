46a838aba814bb14be003cd5657f2fd6e3f4c4d6
A00295213 <156947116+A00295213@users.noreply.github.com>
2024-03-29 01:29:44 +0000

    Decorator

---
+++ newApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+
+class Program 
+{
+    static void Main (string[] args)
+    {
+        ConcreteComponent concreateComponent = new ConcreateComponent();
+        PlainDecorator plainDecorator = new PlainDecorator();
+        UppercaseDecorator uppercaseDecorator = new UppercaseDecorator();
+        ColorDecorator colorDecorator = new ColorDecorator();
+
+
+        Console.WriteLine("Original String:" +concreateComponent.GetText());
+        Console.WriteLine("Plain decorator:" +plainDecorator.GetText());
+        Console.WriteLine("Upper case Decorator:" +uppercaseDecorator.GetText());
+        Console.WriteLine("Color Decorator:" +colorDecorator.GetText());
+    }
+}
