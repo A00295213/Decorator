1ed1eb58937917836cc9e421dc2c07c29d7c4405
A00295213 <156947116+A00295213@users.noreply.github.com>
2024-03-29 01:47:28 +0000

    Decorator

--- newApp/Program.cs
+++ newApp/Program.cs
@@ -6,10 +6,10 @@ class Program
 {
     static void Main (string[] args)
     {
-        ConcreteComponent concreateComponent = new ConcreateComponent();
-        PlainDecorator plainDecorator = new PlainDecorator();
-        UppercaseDecorator uppercaseDecorator = new UppercaseDecorator();
-        ColorDecorator colorDecorator = new ColorDecorator();
+        var concreateComponent = new ConcreateComponent();
+        var plainDecorator = new PlainDecorator();
+        var uppercaseDecorator = new UppercaseDecorator();
+        var colorDecorator = new ColorDecorator();
 
 
         Console.WriteLine("Original String:" +concreateComponent.GetText());
